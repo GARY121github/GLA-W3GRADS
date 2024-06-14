321e7ff95016f30d2eabf8098a117882af9cddc1
gary <harshprakash000@gmail.com>
2024-06-15 00:46:39 +0530

    Fixed the First_Missing_Positive_41.java code

--- src/DAY_03/First_Missing_Positive_41.java
+++ src/DAY_03/First_Missing_Positive_41.java
@@ -1,25 +1,25 @@
 package DAY_03;
 
-import java.util.Arrays;
-
 public class First_Missing_Positive_41 {
     public static void main(String[] args) {
-
+        int[] arr = {10 , 20 , 30};
+        System.out.println(firstMissingPositive(arr));
     }
-    public int firstMissingPositive(int[] arr) {
+    static int firstMissingPositive(int[] arr) {
         boolean isOnePresent = false;
 
         for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] < 0 || arr[i] > arr.length){
+            if(arr[i] <= 0 || arr[i] > arr.length){
                 arr[i] = 1;
             } else if (arr[i] == 1) {
                 isOnePresent = true;
             }
         }
 
-        if(isOnePresent){
+        if(!isOnePresent){
             return 1;
         }
+
 
         for(int i = 0 ; i < arr.length ; i++){
             int index = Math.abs(arr[i]) - 1;
@@ -36,8 +36,6 @@ public class First_Missing_Positive_41 {
                 break;
             }
         }
-
-        Arrays.sort(arr);
-        return 1;
+        return ans == 0 ? arr.length + 1 : ans;
     }
 }
